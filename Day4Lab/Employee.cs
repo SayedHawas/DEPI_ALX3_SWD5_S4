b075423fdc8900f993b6134cc46afa6aa4e7d9bf
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-07-25 17:04:37 +0300

    Update Part 2

---
+++ Day4Lab/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace Day4Lab
+{
+    public class Employee
+    {
+        //Class Members
+        //Class Field
+        private int _id;
+        //Id , Name , Title , Salary , Hiredate , DOB 
+        //Property Full
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        //Auto Property 
+        public string Name { get; set; }
+
+        //Method 
+
+    }
+}
