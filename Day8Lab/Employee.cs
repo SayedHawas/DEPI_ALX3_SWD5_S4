64dd8c60e587f0b14b7762c67899d11162b1b421
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-08-11 22:35:41 +0300

    update

--- Day8Lab/Employee.cs
+++ Day8Lab/Employee.cs
@@ -4,8 +4,33 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Job { get; set; }
+        public double Salary { get; set; }
+        public bool Attend { get; set; }
         public int Age { get; set; }
 
+        public static List<Employee> ListEmployees = new List<Employee>() {
+            new Employee { Id = 1, Name = "Retaj", Job = "Developer", Salary = 5000, Attend = true },
+            new Employee { Id = 2, Name = "Mariam", Job = "Dr", Salary = 6000, Attend = true },
+            new Employee { Id = 3, Name = "sayed", Job = "Developer", Salary = 3000, Attend = false },
+            new Employee { Id = 4, Name = "osama", Job = "eng", Salary = 7000, Attend = true },
+         };
+
+        public static void Report(List<Employee> list, MyConditionHandler conditionHandler)
+        {
+            foreach (Employee emp in list)
+            {
+                //Hard code
+                //if (emp.Salary > 4000)
+                if (conditionHandler(emp))
+                {
+                    Console.WriteLine($"the Id {emp.Id} name {emp.Name} salary {emp.Salary} job {emp.Job} Attend {emp.Attend}");
+                }
+
+            }
+        }
+
+        public delegate bool MyConditionHandler(Employee emp);
 
 
 
