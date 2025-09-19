12cc5a9d10a05322ce07eac550342f85dbd6ef69
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-09-19 17:36:30 +0300

    Update

---
+++ Day16LabWebAPI8Demo/Controllers/DemosController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day16LabWebAPI8Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemosController : ControllerBase
+    {
+        private static List<string> Names = new List<string>() { "Ahmed", "Ali", "Sayed", "Mariem", "Retaj" };
+
+
+        public DemosController()
+        {
+
+        }
+
+        // https://localhost:7272/api/demos
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Names;
+        }
+        // https://localhost:7272/api/demos/1  RouteValue
+        [HttpGet("{id:int}")]
+        //[Route("{id}")]
+        public string GetByID(int id)
+        {
+            return Names[id];
+        }
+        [HttpGet("{name:alpha}")]
+        public string GetByName(string name)
+        {
+            return Names.FirstOrDefault(a => a == name);
+        }
+
+        [HttpGet("/api/forname/{name}")]
+        public string GetByFullName(string name)
+        {
+            return Names.FirstOrDefault(a => a == name);
+        }
+        [HttpPost]
+        public string Create(string newName)
+        {
+            Names.Add(newName);
+            return "Save Done ...";
+        }
+    }
+}
---
+++ Day16LabWebAPI9Demo/Controllers/DepartmentsController.cs
@@ -0,0 +1,72 @@
+﻿using Day16LabWebAPI9Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day16LabWebAPI9Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly WebAPIDBContext _DB;
+
+        public DepartmentsController(WebAPIDBContext db)
+        {
+            this._DB = db;
+        }
+
+        //Get 
+        [HttpGet]
+        //public IEnumerable<Department> GetAll()
+        public IActionResult GetAll()
+        {
+            return Ok(_DB.Departments.ToList());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            var dep = _DB.Departments.Find(id);
+            if (dep == null) return NotFound();
+            else return Ok(dep);
+        }
+        [HttpPost]
+        public IActionResult Create(Department newDepartment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(newDepartment);
+            }
+            else
+            {
+                _DB.Departments.Add(newDepartment);
+                _DB.SaveChanges();
+                return Created();
+            }
+        }
+        [HttpPut]
+        public IActionResult Edit(Department updateDepartment, int id)
+        {
+            var dep = _DB.Departments.Find(id);
+            if (dep == null) return BadRequest();
+            else
+            {
+                dep.Name = updateDepartment.Name;
+                dep.Location = updateDepartment.Location;
+                dep.ManagerName = updateDepartment.ManagerName;
+                _DB.SaveChanges();
+                return NoContent();
+            }
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var dep = _DB.Departments.Find(id);
+            if (dep == null) return BadRequest();
+            else
+            {
+                _DB.Departments.Remove(dep);
+                _DB.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
